df8df884602df61573892eac68c77e9b40deeccc
Abhishek Baiju <johnponnamkudam@gmail.com>
2023-06-04 12:06:59 +0530

    removed tqdm
    Removed TQDM as it interferes with single video downloading, also removed ":" for "_" in filenaming

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
 import os
 from pytube import Playlist, YouTube
-import urllib.parse
-from tqdm import tqdm
 
 
 def download_video(url, folder, number_videos, skip_existing):
     try:
-        playlist = Playlist(url)
-        videos = playlist.video_urls
+        if 'playlist' in url:
+            playlist = Playlist(url)
+            videos = playlist.video_urls
+        else:
+            videos = [url]
 
         download_path = os.path.join(os.getcwd(), folder)
         os.makedirs(download_path, exist_ok=True)
 
-        for i, video in enumerate(tqdm(videos, desc="Downloading"), start=1):
+        for i, video in enumerate(videos, start=1):
             youtube = YouTube(video)
             stream = youtube.streams.get_highest_resolution()
 
             if number_videos:
-                title = f'{i:02d} : {youtube.title}'
+                title = f'{i:02d} - {youtube.title}'
             else:
                 title = youtube.title
 
-            filename = f'{title}.{stream.subtype}'
+            filename = f'{title}.{stream.subtype}'.replace(':', '_')
 
             file_path = os.path.join(download_path, filename)
             if skip_existing and os.path.exists(file_path):
-                tqdm.write(f'Skipping existing file: {filename}')
+                print(f'Skipping existing file: {filename}')
                 continue
 
             stream.download(output_path=download_path, filename=filename)
 
-            tqdm.write(f'Downloaded video {i}/{len(videos)}: {filename}')
+            print(f'Downloaded video {i}/{len(videos)}: {filename}')
 
-        print('Download complete!')
+        print('Beeep boop! Download complete!')
     except Exception as e:
         print('Error:', str(e))
 
 
 def main():
-    print('beepboop..')
+    print('Beeep boop! Welcome to the YouTube video/playlist downloader app!')
 
     url = input('Enter the YouTube video or playlist link: ')
     folder = input('Enter the download folder name: ')
